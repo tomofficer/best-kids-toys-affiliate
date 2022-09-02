fb94aadb190c41f226a94eab4a346c3a1a074c03
Tom Officer <thomasofficerjr@gmail.com>
2022-09-01 21:55:50 -0700

    added header, added footer

--- src/App.js
+++ src/App.js
@@ -9,32 +9,22 @@ import {
   Grid,
   theme,
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import { ColorModeSwitcher } from './components/ColorModeSwitcher';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
+      <Header />
+
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
+          {/* <ColorModeSwitcher justifySelf="flex-end" /> */}
+          <VStack spacing={8}></VStack>
         </Grid>
       </Box>
+      <Footer />
     </ChakraProvider>
   );
 }
