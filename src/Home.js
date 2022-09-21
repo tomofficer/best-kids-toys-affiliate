8c1f4f5278a236a7c79ca33d2545901559ab0afa
Tom Officer <thomasofficerjr@gmail.com>
2022-09-20 22:27:51 -0700

    color switcher testing

--- src/Home.js
+++ src/Home.js
@@ -16,14 +16,14 @@ const Home = () => {
     <div>
       <Hero2 />
 
-      {/* <ProductMap /> */}
+      <ProductMap />
       <SubFooter />
 
       <MainArticle />
 
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
-          {/* <ColorModeSwitcher justifySelf="flex-end" /> */}
+          <ColorModeSwitcher justifySelf="flex-end" />
           <VStack spacing={8}>
             <Hero />
             <SubFooter />
