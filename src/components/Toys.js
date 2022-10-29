533321908cb91e260aa019b3a1c63d32b465223e
Tom Officer <thomasofficerjr@gmail.com>
2022-10-29 01:38:32 -0700

    added product url navigation, updated product map styles

--- src/components/Toys.js
+++ src/components/Toys.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Flex, Box, chakra, Image } from '@chakra-ui/react';
+import { Flex, Box, chakra, Image, Link } from '@chakra-ui/react';
 
 const Toys = () => {
   //data set for toys
@@ -12,6 +12,22 @@ const Toys = () => {
       img: 'https://ik.imagekit.io/v66nb6oaq/Best_Kids_Toys/habaeggs_evphepGx_.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1667028754580',
       url: 'https://www.amazon.com/HABA-Musical-Eggs-Acoustic-Germany/dp/B00BF3D666/ref=sr_1_7?crid=2Z1KKBC0NWY2R&keywords=haba&qid=1667028707&qu=eyJxc2MiOiI3LjUyIiwicXNhIjoiNy4yMCIsInFzcCI6IjYuMzYifQ%3D%3D&sprefix=natural+baby+toys%2Caps%2C456&sr=8-7',
     },
+    {
+      category: '',
+      title: 'Haba Blocks',
+      description:
+        'Each cube awaits with a different acoustic effect! In this way, early perception and musicality are fostered.',
+      img: 'https://ik.imagekit.io/v66nb6oaq/Best_Kids_Toys/habablocks_Xo7IvvlL9.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1667031920901',
+      url: 'https://www.amazon.com/HABA-Sounds-Discovery-Acoustic-Germany/dp/B00BF3CK90/ref=sxin_15_pa_sp_search_thematic_sspa?content-id=amzn1.sym.9c246b91-3d49-4e6c-8ff6-5b6e9a100bd4%3Aamzn1.sym.9c246b91-3d49-4e6c-8ff6-5b6e9a100bd4&crid=2Z1KKBC0NWY2R&cv_ct_cx=haba&keywords=haba&pd_rd_i=B00BF3CK90&pd_rd_r=8b1fc94a-982c-41d6-a224-42d256b66454&pd_rd_w=toMui&pd_rd_wg=twxJ8&pf_rd_p=9c246b91-3d49-4e6c-8ff6-5b6e9a100bd4&pf_rd_r=VZYB5Q5JRES9W4E2N8XY&qid=1667031865&qu=eyJxc2MiOiI3LjUyIiwicXNhIjoiNy4yMCIsInFzcCI6IjYuMzYifQ%3D%3D&sprefix=natural+baby+toys%2Caps%2C456&sr=1-2-a43b4223-fbe9-48b0-af69-6d70cf84978b-spons&psc=1&smid=A1V3CN8LT5ZFSV',
+    },
+    {
+      category: '',
+      title: 'Agirgle Building Blocks',
+      description:
+        'Large 8 piece rainbow building block set! Promotes baby awareness of shape and color as well as helps build coordination.',
+      img: 'https://ik.imagekit.io/v66nb6oaq/Best_Kids_Toys/agirgleblocks_JQmdIw2fS.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1667032404960',
+      url: 'https://www.amazon.com/dp/B09JFW3YBT/ref=sspa_dk_detail_6?psc=1&pd_rd_i=B09JFW3YBT&pd_rd_w=4GLlH&content-id=amzn1.sym.bff6e147-54ad-4be3-b4ea-ec19ea6167f7&pf_rd_p=bff6e147-54ad-4be3-b4ea-ec19ea6167f7&pf_rd_r=EPP9TAVQ9AWEG0K09VTG&pd_rd_wg=mVae6&pd_rd_r=c3c09365-cb8e-4b57-9c9f-d40c809be1b8&s=toys-and-games&sp_csd=d2lkZ2V0TmFtZT1zcF9kZXRhaWwy',
+    },
   ];
 
   return (
@@ -19,17 +35,17 @@ const Toys = () => {
       {toyData.map(toy => (
         <>
           <Flex
-            bg="#edf3f8"
-            _dark={{
-              bg: '#3e3e3e',
-            }}
-            p={50}
+            // bg="#edf3f8"
+            // _dark={{
+            //   bg: '#3e3e3e',
+            // }}
+            p={30}
             w="full"
             alignItems="center"
             justifyContent="center"
           >
             <Box
-              maxW="xs"
+              maxW="md"
               mx="auto"
               bg="white"
               _dark={{
@@ -38,7 +54,7 @@ const Toys = () => {
               shadow="lg"
               rounded="lg"
             >
-              <Box px={4} py={2}>
+              <Box px={4} py={2} bg="gray.100" borderTopRadius={10}>
                 <chakra.h1
                   color="gray.800"
                   _dark={{
@@ -63,10 +79,12 @@ const Toys = () => {
               </Box>
 
               <Image
-                h={48}
+                h="full"
                 w="full"
                 fit="cover"
                 mt={2}
+                py={6}
+                px={8}
                 src={toy.img}
                 alt="NIKE AIR"
               />
@@ -75,32 +93,34 @@ const Toys = () => {
                 alignItems="center"
                 justifyContent="center"
                 px={4}
-                py={4}
+                py={6}
                 bg="blue.700"
                 roundedBottom="lg"
               >
                 {/* <chakra.h1 color="white" fontWeight="bold" fontSize="lg">
                   $129
                 </chakra.h1> */}
-                <chakra.button
-                  px={4}
-                  py={2}
-                  bg="white"
-                  fontSize="xs"
-                  color="black"
-                  fontWeight="bold"
-                  rounded="lg"
-                  textTransform="uppercase"
-                  _hover={{
-                    bg: 'yellow.300',
-                    color: 'blue.800',
-                  }}
-                  _focus={{
-                    bg: 'gray.400',
-                  }}
-                >
-                  View On Amazon
-                </chakra.button>
+                <Link href={toy.url} target="_blank">
+                  <chakra.button
+                    px={4}
+                    py={2}
+                    bg="white"
+                    fontSize="xs"
+                    color="black"
+                    fontWeight="bold"
+                    rounded="lg"
+                    textTransform="uppercase"
+                    _hover={{
+                      bg: 'yellow.300',
+                      color: 'blue.800',
+                    }}
+                    _focus={{
+                      bg: 'gray.400',
+                    }}
+                  >
+                    View On Amazon
+                  </chakra.button>
+                </Link>
               </Flex>
             </Box>
           </Flex>
